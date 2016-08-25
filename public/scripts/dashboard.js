aecef2327464882f15c1d61076ee5651d3a883bd
chriselfering <chriselfering@gmail.com>
2016-08-25 15:29:40 -0600

    routing stuff

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,19 +1,19 @@
 angular.module('app.dashboard', [])
     .controller('DashboardController', Dashboard);
-
-function Dashboard() {
-    console.info('Dashboard.initialized')
-}
-$routeProvider.when('/goals', {
-    templateUrl: '/views/goals.html',
-});
-$routeProvider.when('/habits', {
-    templateUrl: '/views/habits.html'
-});
-$routeProvider.when('/lists', {
-    templateUrl: '/views/lists.html'
-});
-$routeProvider.when('/login', {
-    templateUrl: '/views/auth.html',
-    controller: 'AuthController as auth'
-});
+    .config(function($routeProvider) {
+        function Dashboard() {
+            console.info('Dashboard.initialized')
+        }
+        $routeProvider.when('/goals', {
+            templateUrl: '/views/goals.html',
+        });
+        $routeProvider.when('/habits', {
+            templateUrl: '/views/habits.html'
+        });
+        $routeProvider.when('/lists', {
+            templateUrl: '/views/lists.html'
+        });
+        $routeProvider.when('/charts', {
+            templateUrl: '/views/charts.html'
+        });
+    });
